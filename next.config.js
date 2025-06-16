d44926ee6ab207d4593c9704697516b89645c487
XipleETH <axiesugar@gmail.com>
2025-06-16 15:01:39 -0500

    Fix Vercel build issues: remove CUSTOM_KEY, exclude functions dir, clean package.json

--- next.config.js
+++ next.config.js
@@ -5,17 +5,22 @@ const nextConfig = {
   images: {
     domains: ['ipfs.io', 'gateway.pinata.cloud'],
   },
+  // Exclude functions directory and other non-frontend files
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
   webpack: (config) => {
     config.resolve.fallback = {
       fs: false,
       net: false,
       tls: false,
     };
+    // Exclude functions directory from webpack
+    config.module.rules.push({
+      test: /functions\//,
+      loader: 'ignore-loader'
+    });
     return config;
   },
-  env: {
-    CUSTOM_KEY: process.env.CUSTOM_KEY,
-  },
+
 }
 
 module.exports = nextConfig 
